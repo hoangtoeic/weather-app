4317749760e7bb402ee3fff88cb3df0bac8287ee
hoangtoeic <mr.hoangenglish@gmail.com>
2021-10-24 16:10:46 +0700

    weather_test_1

---
+++ app_copy.js
@@ -0,0 +1,8 @@
+const request = require('request')
+const geocode = require('./utils/geocode')
+
+
+geocode('Boston', (error, data) => {
+    console.log('Error', error)
+    console.log('Data', data)
+})
